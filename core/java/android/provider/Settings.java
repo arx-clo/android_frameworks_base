5854b27e24c68d1f151e8271d7a7e1e1d701847e
Wrdn28 <ifloraxd@gmail.com>
2025-11-07 23:24:40 +0000

    Revert "SystemUI: Allow disabling screenshot shutter sound [2/2]"
    This reverts commit 3a09c47fbe96409d5bac3129fcaf50083a4de6fe.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5632,12 +5632,6 @@ public final class Settings {
         public static final String ENABLE_FLOATING_ROTATION_BUTTON = "enable_floating_rotation_button";
 
         /**
-         * Whether to play the camera shutter sound on taking a screenshot.
-         * @hide
-         */
-        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
-
-        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
